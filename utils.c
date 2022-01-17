5c966806670997c7c3e84ef917f329d264ce49e6
Chloe DAVEUX <cdaveux@e1r10p13.42.fr>
2022-01-17 16:36:37 +0100

    cleanup

--- utils.c
+++ utils.c
@@ -24,3 +24,30 @@ void	ft_putnbr(int nb)
 		ft_putchar(nb1 % 10 + 48);
 	}
 }
+
+int	ft_atoi(const char *str)
+{
+	int long	nb;
+	int			sign;
+	int			i;
+
+	nb = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == 32 || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == '\n')
+		i++;
+	if (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= 48 && str[i] <= 57))
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	nb *= sign;
+	return (nb);
+}
